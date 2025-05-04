3f442e8e3ceb8a3f38a73f0d214a9b379160330e
frazer1449 <frazer1449@gmail.com>
2025-05-04 23:16:08 +0900

    May 4, 2025

--- src/main/java/com/example/test2/MainController.java
+++ src/main/java/com/example/test2/MainController.java
@@ -7,22 +7,22 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MainController {
 //    명령어 가져오기
-    @GetMapping("/hello")
-    @ResponseBody
-    public String hello(){
-        return "Hello World";
-    }
-    @GetMapping("/sum")
-    @ResponseBody
-    public String sum(){
-
-        int sum = 0;
-        for (int i = 1; i <= 20; i++){
-            sum += i;
-        }
-        String s = "" + sum;
-        return s;
-    }
+//    @GetMapping("/hello")
+//    @ResponseBody
+//    public String hello(){
+//        return "Hello World";
+//    }
+//    @GetMapping("/sum")
+//    @ResponseBody
+//    public String sum(){
+//
+//        int sum = 0;
+//        for (int i = 1; i <= 20; i++){
+//            sum += i;
+//        }
+//        String s = "" + sum;
+//        return s;
+//    }
 //
     @GetMapping("/")
     public String root() {
